3b2e10361ae7a8743b87b6dbabd287e21ce111c3
Clara <claraagv54@gmail.com>
2025-08-10 14:45:50 -0300

    FooterComponent

--- frontend/dompolski-frontend/src/App.vue
+++ frontend/dompolski-frontend/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import HeaderComponent from './components/HeaderComponent.vue'
-
+import FooterComponent from './components/FooterComponent.vue'
 </script>
 
 <template>
@@ -13,7 +13,7 @@ import HeaderComponent from './components/HeaderComponent.vue'
     <main>
       <RouterView />
     </main>
-
+<FooterComponent></FooterComponent>
   </div>
 
 
---
+++ frontend/dompolski-frontend/src/components/FooterComponent.vue
@@ -0,0 +1,14 @@
+<template> 
+    <footer>
+        <p>© 2025 Sociedad Cultural y Deportiva; Union Polaca y Seccion Ex Combatientes. Todos los derechos reservados.</p>
+    </footer>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+
+</style>
