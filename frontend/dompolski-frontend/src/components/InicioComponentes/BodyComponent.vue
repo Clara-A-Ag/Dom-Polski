fb8bcf1cacf48645b4c4e489538f98bd8324b9f7
Clara <claraagv54@gmail.com>
2025-08-11 17:22:18 -0300

    imagenes body

--- frontend/dompolski-frontend/src/components/InicioComponentes/BodyComponent.vue
+++ frontend/dompolski-frontend/src/components/InicioComponentes/BodyComponent.vue
@@ -3,6 +3,7 @@
     <div class="carousel-track" :style="trackStyle">
 <img v-for="(image, index) in images" :key="index" :src="image.src" :alt="image.alt"> //v-for itera sobre el arreglo de images para renderizar cada imagen.
     </div>
+<h1 class="carrusel-title">Dom Polski - Santa Fe</h1>
 </div>
 
 </template>
@@ -12,8 +13,10 @@ export default {
   data() {
     return {
       images: [
-        { src: '/imagenes/flag-792067.jpg', alt:'Imagen de la bandera polaca' },
-        {src: '/imagenes/SantaFe14.jpg', alt: 'Imagen de Dom Polski Frente'},
+      {src: '/imagenes/dompofrente.jpg', alt: 'Imagen de Dom Polski Frente'},  
+      { src: '/imagenes/flag-792067.jpg', alt:'Imagen de la bandera polaca' },
+        {src: '/imagenes/dompocartel.jpg', alt: 'Imagen de Dom Polski Cartel'},
+        {src: '/imagenes/haciendopierogi.jpeg', alt: 'Imagen de Dom Polski haciendo Pierogi'},
         { src:'/imagenes/wawel-3683040.jpg', alt: 'Imagen del Castillo de Wawel' },
         { src: '/imagenes/old-town-5062333.jpg', alt: 'Ciudad Polaca' },
         { src: '/imagenes/IMG_9846.jpeg', alt: 'Imagen del Ballet Aguila Blanca'},
@@ -59,7 +62,9 @@ export default {
 <style scoped>
 .carousel-container {
   width: 100%;
+  margin: 0 auto;
   overflow: hidden;
+  position: relative; /* ¡IMPORTANTE! Posicionamiento relativo para que los hijos absolutos se posicionen con respecto a este contenedor. */
 }
 
 .carousel-track {
@@ -69,8 +74,21 @@ export default {
 
 .carousel-track img {
   width: 100%;
-  height: 700px;
+  height: auto;
   flex-shrink: 0;
+  object-fit: cover;
 }
-
+.carrusel-title{
+  position: absolute; /* Posicionamiento absoluto para colocar el título sobre las imágenes. */
+  top: 75%; /* Ajusta según sea necesario */
+  left: 50%;
+  transform: translateX(-50%);
+  color: white;
+ text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7); /* Sombra para mejorar la legibilidad */
+  padding: 10px 20px;
+  border-radius: 5px;
+  font-size: 24px;
+  text-align: center;
+  z-index: 10; /* Asegura que el título esté por encima de las imágenes */
+}
 </style>
