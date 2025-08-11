f2d70180b3cf69500b7750f3454949e011837c5c
Clara <claraagv54@gmail.com>
2025-08-11 19:42:49 -0300

    haciendo la descripcion

--- frontend/dompolski-frontend/src/components/InicioComponentes/BodyComponent.vue
+++ frontend/dompolski-frontend/src/components/InicioComponentes/BodyComponent.vue
@@ -6,11 +6,11 @@
 <h1 class="carrusel-title">Dom Polski - Santa Fe</h1>
 </div>
 <div class="Descripcion">
-  <a>¡Bienvenidos a Dom Polski Santa Fe! Somos una asociación civil sin fines de lucro que busca promover la cultura polaca en Argentina. Nuestro objetivo es preservar y difundir las tradiciones, costumbres y valores de la comunidad polaca en Santa Fe y sus alrededores.</a>
-  <li>En Dom Polski, organizamos eventos culturales, clases de idioma polaco, talleres de cocina tradicional y actividades recreativas para todas las edades. Nuestro espacio está abierto a todos aquellos interesados en conocer más sobre la rica herencia cultural polaca.</li>
-  <li>¡Únete a nosotros y descubre la magia de la cultura polaca en Santa Fe!</li>
-  <a href="https://share.google/c6dt38SD54IBFe4TN">Av. Aristóbulo del Valle 5444, Santa Fe, Argentina, 3000</a>
-  <li>Sociedad Cultural y Deportiva; Union Polaca y Seccion Ex Combatientes</li>
+  <p class="bienvenidos">¡Bienvenidos a Dom Polski Santa Fe!</p> 
+  <p>Somos una asociación civil sin fines de lucro que busca promover la cultura polaca en Argentina. Nuestro objetivo es preservar y difundir las tradiciones, costumbres y valores de la comunidad polaca en Santa Fe y sus alrededores.</p>
+  <p>En Dom Polski, organizamos eventos culturales, clases de idioma polaco, talleres de cocina tradicional y actividades recreativas para todas las edades. Nuestro espacio está abierto a todos aquellos interesados en conocer más sobre la rica herencia cultural polaca.</p>
+  <p>¡Únete a nosotros y descubre la magia de la cultura polaca en Santa Fe!</p>
+  <br><a>Dirección: </a><a href="https://share.google/c6dt38SD54IBFe4TN">Av. Aristóbulo del Valle 5444, Santa Fe, Argentina, 3000</a>
 
 </div>
 
@@ -23,7 +23,6 @@ export default {
       images: [
       {src: '/imagenes/dompofrente.JPG', alt: 'Imagen de Dom Polski Frente'},  
       { src: '/imagenes/flag-792067.jpg', alt:'Imagen de la bandera polaca' },
-      {src: '/imagenes/bandargentina.jpg', alt: 'Imagen de la bandera Argentina'},
         {src: '/imagenes/dompocartel.JPG', alt: 'Imagen de Dom Polski Cartel'},
       { src:'/imagenes/wawel-3683040.jpg', alt: 'Imagen del Castillo de Wawel' },
       {src: '/imagenes/haciendopierogi.jpeg', alt: 'Imagen de Dom Polski haciendo Pierogi'},
@@ -86,6 +85,7 @@ export default {
   height: 600px;
   flex-shrink: 0;
   object-fit: cover;
+  
 }
 .carrusel-title{
   position: absolute; /* Posicionamiento absoluto para colocar el título sobre las imágenes. */
@@ -101,11 +101,21 @@ export default {
   z-index: 10; /* Asegura que el título esté por encima de las imágenes */
 }
 .Descripcion {
-  margin: 20px auto;
-  padding: 20px;
-  background-color: #a7c1e3;
-  border-radius: 8px;
+  background-color: #e2e4e8;
   font-style: 'lora', serif;
   font-weight: solid;
+  padding-top: 15px;
+  text-align: center;
+  padding-left: 5px;
+  padding-right: 5px;
+  padding-bottom: 15px;
+}
+p{
+  margin-bottom: 15px;
+}
+.bienvenidos{
+  font-size: 20px;
+  font-weight: bold;
+  color:#c00606;
 }
 </style>
